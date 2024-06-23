dd2857326928d0d2c05cf898c2fa62d95d92aa7f
mansi <mansi.valani@gmail.com>
2024-06-23 21:39:16 +0530

    Updated Order to add Circuit breaker

--- src/main/java/com/digitalshop/OrderService/external/feignclient/PaymentService.java
+++ src/main/java/com/digitalshop/OrderService/external/feignclient/PaymentService.java
@@ -1,11 +1,13 @@
 package com.digitalshop.OrderService.external.feignclient;
 
+import com.digitalshop.OrderService.exception.CustomException;
 import com.digitalshop.OrderService.external.request.PaymentRequest;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
+@CircuitBreaker(name="external", fallbackMethod = "fallback")
 @FeignClient(name = "PAYMENTSERVICE/payment" )
 public interface PaymentService {
 
@@ -13,5 +15,9 @@ public interface PaymentService {
         public ResponseEntity<Long> doPayment(@RequestBody PaymentRequest paymentRequest);
 
 
-
+        default ResponseEntity<Long> fallback(Exception e) {
+                throw new CustomException("Payment Service is not available",
+                        "UNAVAILABLE",
+                        500);
+        }
 }
--- src/main/java/com/digitalshop/OrderService/external/feignclient/ProductService.java
+++ src/main/java/com/digitalshop/OrderService/external/feignclient/ProductService.java
@@ -1,5 +1,7 @@
 package com.digitalshop.OrderService.external.feignclient;
 
+import com.digitalshop.OrderService.exception.CustomException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,9 +9,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@CircuitBreaker(name="external", fallbackMethod = "fallback")
 @FeignClient(name="PRODUCTSERVICE/product")
 public interface ProductService {
 
     @PutMapping("/reduceQuantity/{id}")
     public ResponseEntity<?> reduceQuantity(@PathVariable long id, @RequestParam long quantity);
+
+    default ResponseEntity<Long> fallback(Exception e) {
+        throw new CustomException("Product Service is not available",
+                "UNAVAILABLE",
+                500);
+    }
 }
